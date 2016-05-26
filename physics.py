a24d5a0e41e6f016e00aef96c74880d67f0b843f
Kathryn <kaththedragon@gmail.com>
2016-05-26 14:06:18 +0100

    Added CVector and PVector subclasses of Vector to allow for cartesian and polar coordinates.

--- physics.py
+++ physics.py
@@ -1,20 +1,21 @@
-﻿class Vector():
+﻿from math import cos, sin, asin, atan2
+
+class Vector():
     def __init__(self, *args):
         if len(args) == 0:
             self.x = 0
             self.y = 0
             self.z = 0
-        elif len(args) == 1:
+            self.r = 0
+            self.theta = 0
+            self.phi = 0
+        else:
             self.x = args[0].x
             self.y = args[0].y
             self.z = args[0].z
-        else:
-            self.x = args[0]
-            self.y = args[1]
-            if len(args) == 2:
-                self.z = 0
-            else:
-                self.z = args[2]
+            self.r = args[0].r
+            self.theta = args[0].theta
+            self.phi = args[0].phi
         return
     
     def __repr__(self):
@@ -30,77 +31,114 @@
         return hash((self.x, self.y, self.z))
 
     def __add__(self, vect):
-        return Vector(
+        return CVector(
             self.x + vect.x,
             self.y + vect.y,
             self.z + vect.z
         )
         
     def __sub__(self, vect):
-        return Vector(
+        return CVector(
             self.x - vect.x,
             self.y - vect.y,
             self.z - vect.z
         )
         
-    def __mul__(self, val):
-        return Vector(
-            self.x * val,
-            self.y * val,
-            self.z * val
-        )
+    def __mul__(self, arg):
+        if isinstance(arg, Vector):
+            return CVector(
+                self.y*arg.z - self.z*arg.y,
+                self.z*arg.x - self.x*arg.z,
+                self.x*arg.y - self.y*arg.x
+            )
+        else:
+            return PVector(
+                self.r * arg,
+                self.theta,
+                self.phi
+            )
         
     def __rmul__(self, val):
-        return Vector(
-            self.x * val,
-            self.y * val,
-            self.z * val
+        return PVector(
+            self.r * val,
+            self.theta,
+            self.phi
         )
         
     def __div__(self, val):
-        return Vector(
-            self.x / val,
-            self.y / val,
-            self.z / val
+        return PVector(
+            self.r / val,
+            self.theta,
+            self.phi
         )
         
     def __neg__(self):
-        return Vector(
-            -vect.x,
-            -vect.y,
-            -vect.z
+        return PVector(
+            -self.r,
+            self.theta,
+            self.phi
         )
         
     def __pos__(self):
-        return Vector(
-            vect.x,
-            vect.y,
-            vect.z
-        )
+        return Vector(self)
     
     def mag(self):
-        return (self.x**2 + self.y**2 + self.z**2)**0.5
+        return self.r
     
     def norm(self):
-        return self / self.mag()
+        return PVector(
+            1,
+            self.theta,
+            self.phi
+        )
         
     def setMag(self, mag):
-        return self.norm() * mag
+        return PVector(
+            mag,
+            self.theta,
+            self.phi
+        )
         
     def dot(self, vect):
         return self.x * vect.x + self.y * vect.y + self.z * vect.z
-    
-    def cross(self, *args):
-        ans = Vector(self)
-        for arg in args:
-            x = ans.y*arg.z - ans.z*arg.y
-            y = ans.z*arg.x - ans.x*arg.z
-            z = ans.x*arg.y - ans.y*arg.x
-            ans.x = x
-            ans.y = y
-            ans.z = z
-        return ans
 
+class CVector(Vector):
+    def __init__(self, *args):
+        if len(args) > 1:
+            self.x = args[0]
+            self.y = args[1]
+            self.r = self.x**2 + self.y**2
+            self.theta = atan2(self.y, self.x)
+            if len(args) == 2 or not args[2]:
+                self.z = 0
+                self.r **= 0.5
+                self.phi = 0
+            else:
+                self.z = args[2]
+                self.r = (self.r + self.z**2)**0.5
+                self.phi = asin(self.z / self.r)
+        else:
+            super().__init__(*args)
+        return
+
+class PVector(Vector):
+    def __init__(self, *args):
+        if len(args) > 1:
+            self.r = args[0]
+            self.theta = args[1]
+            self.x = self.r * cos(self.theta)
+            self.y = self.r * sin(self.theta)
+            if len(args) == 2 or not args[2]:
+                self.phi = 0
+                self.z = 0
+            else:
+                self.phi = args[2]
+                self.x *= cos(self.phi)
+                self.y *= cos(self.phi)
+                self.z = self.r * sin(self.phi)
+        else:
+            super().__init__(*args)
+        return
 
 class Body():
     def __init__(self, *args):
